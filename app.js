b8fb7891c292311bca2f65aa4ec42e869803a358
jayson reyes <jreyes216@outlook.com>
2017-05-18 01:15:07 +0800

    second commit

--- app.js
+++ app.js
@@ -3,17 +3,42 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 
+
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 
-app.use(bodyParser());
+
 app.use(express.static(path.join(__dirname,'bower_components')));
+app.use(bodyParser());
+
+var todoItem = [
+	{id: 1, desc:'foo'},
+	{id: 1, desc:'bar'},
+	{id: 1, desc:'baz'}
+]
+
+app.get('/',function(req,res){
+	res.render('index', {
+		title: 'My App',
+		items: todoItem
+	});
+});
+
+app.post('/add', function(req,res){
+	var newItem = req.body.newItem;
+
+	todoItem.push({
+		id: todoItem.length+1,
+		desc: newItem
+	});
+
+	res.redirect('/');
+});
 
 
 ////
 
-app.use(require('./todos'));
 ////
 var port = process.env.PORT || 3000;
 
